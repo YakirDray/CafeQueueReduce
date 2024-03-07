254e7d4a6ac7ca4a4573500e6e5889c8720d9c0a
Idantegave7 <158479599+Idantegave7@users.noreply.github.com>
2024-03-07 12:55:24 +0200

    סיפור משתמש

--- CafeQueueReduce/Components/Fooditem.js
+++ CafeQueueReduce/Components/Fooditem.js
@@ -1,14 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable,StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { AntDesign } from '@expo/vector-icons';
+import MenuItem from './MenuItem';
 
-const comp2 = () => {
+const FoodItem = ({item}) => {
+  const data = [item];
   return (
     <View>
       <Text>idan</Text>
+      {data?.map((item,index) => (
+          <>
+          <Pressable style={styles.itemstyle} key={index}>
+              <Text style={styles.textstyle}>{item?.name} ({item?.items?.length})</Text>
+              <AntDesign name="down" size={20} color="black" />
+          </Pressable>
+
+            {item?.items?.map((item,index) => (
+                <MenuItem key={index} item={item}/>
+            ))}
+          </>
+          
+      ))}
     </View>
+
   )
 }
 
-export default comp2
+export default FoodItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+ 
+  itemstyle: {
+    margin: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'lightblue',
+    padding: 10, 
+    borderRadius: 10,
+  },
+  
+  textstyle:{
+    fontSize: 19,
+    fontWeight: 'bold',
+    color: 'black',
+  }
+
+})
