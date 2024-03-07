49da037628196abc76836ecdd2b74ae7dbe85dbc
ofekp3 <ofekpe@ac.sce.ac.il>
2024-03-07 20:33:27 +0200

    Update Carousal.js

--- CafeQueueReduce/Components/Carousal.js
+++ CafeQueueReduce/Components/Carousal.js
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, View, text, Image } from 'react-native';
+import { ScrollView, StyleSheet, View,text,Image } from 'react-native';
 import React from 'react';
 import { SliderBox } from "react-native-image-slider-box"; // Corrected component name
 import UTcarousel from '../unit_test/UTcarousel';
