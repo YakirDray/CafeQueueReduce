ca89347d08ff927cc67c63b8af9c228d714eb2e8
yakir <158854050+YakirDray@users.noreply.github.com>
2024-03-08 13:38:38 +0200

    בדיקות יחידה הזמנות

--- CafeQueueReduce/app/(home)/order.test.js
+++ CafeQueueReduce/app/(home)/order.test.js
@@ -1,37 +1,34 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react-native';
-import Order from './order'; // הנתיב צריך להיות מתואם למיקום הפקטי של הקובץ
+import Order from './Order'; // הנתיב צריך להיות מתואם למיקום הפקטי של הקובץ
 import moment from 'moment-timezone';
 jest.mock('moment-timezone');
 
 describe('Order Component Tests', () => {
 
   beforeEach(() => {
-    // זיוף התגובה של `moment-timezone` לצורך הבדיקה
     moment.tz.mockReturnValue({
       format: jest.fn().mockReturnValue('01/01/2022 12:00:00'),
     });
   });
 
   it('displays the current time in Israel correctly', () => {
-    const { getByText } = render(<order />);
+    const { getByText } = render(<Order />);
     const expectedTime = '01/01/2022 12:00:00';
     expect(getByText(new RegExp(expectedTime))).toBeTruthy();
   });
 
   it('updates tip amount and displays it when a tip option is pressed', () => {
-    const { getByText } = render(<order />);
+    const { getByText } = render(<Order />);
     const tipAmount = "2₪";
     fireEvent.press(getByText(tipAmount));
     expect(getByText(`please pay ₪2 to your delivery agent at the time of delivery`)).toBeTruthy();
   });
 
   it('cancels the tip and removes the displayed message when cancel is pressed', () => {
-    const { getByText, queryByText } = render(<order />);
+    const { getByText, queryByText } = render(<Order />);
     fireEvent.press(getByText("2₪")); // הוספת טיפ
     fireEvent.press(getByText("(Cancel)")); // לחיצה על ביטול
     expect(queryByText("please pay ₪2 to your delivery agent at the time of delivery")).toBeNull();
   });
-  
-
 });
