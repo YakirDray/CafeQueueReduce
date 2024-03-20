3589d6e2ffa195d28ae69beb543f4c671a2949b2
Nitaybenadmon123 <nitaybe@ac.sce.ac.il>
2024-03-20 23:30:08 +0200

    המשך סידור עבודה(זמני)

--- CafeQueueReduce/app/(admin)/arrangement_table.js
+++ CafeQueueReduce/app/(admin)/arrangement_table.js
@@ -14,4 +14,12 @@ const WeeklyScheduleScreen = () => {
 
   const morningHours = ['8-9', '9-10', '10-11', '11-12', '12-13'];
   const afternoonHours = ['13-14', '14-15', '15-16', '16-17', '17-18'];
+  const handleEmployeeChange = (dayIndex, employeeIndex, text) => {
+    const updatedScheduleData = [...scheduleData];
+    updatedScheduleData[dayIndex].employees[employeeIndex] = text;
+    setScheduleData(updatedScheduleData);
+  };
+
+  
+   
 }
