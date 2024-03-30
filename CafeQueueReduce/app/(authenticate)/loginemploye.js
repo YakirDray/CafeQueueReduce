0cc78fae89727a679531520a3df8098d7bb08d68
Idantegave7 <158479599+Idantegave7@users.noreply.github.com>
2024-03-30 22:20:32 +0300

    עידכון 1.3

--- CafeQueueReduce/app/(authenticate)/loginemploye.js
+++ CafeQueueReduce/app/(authenticate)/loginemploye.js
@@ -42,10 +42,65 @@ const Login = () => {
             placeholder="הזן אימייל"
           />
         </View>
-        
-        
-        
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.input}
+            value={password}
+            onChangeText={setPassword}
+            placeholder="הזן סיסמה"
+            secureTextEntry
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="התחבר" onPress={handleLogin} />
+        </View>
+        <View style={styles.forgotPasswordContainer}>
+          <Button title="שכחתי סיסמא" onPress={() => console.log('Forgot password')} />
+        </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff', // לבן
+    alignItems: 'center',
+  },
+  titleContainer: {
+    marginTop: 50,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  inputContainer: {
+    width: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  inputView: {
+    width: '100%',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+  buttonContainer: {
+    width: '100%',
+    marginBottom: 20,
+  },
+  forgotPasswordContainer: {
+    width: '100%',
+  },
+});
+
+export default Login;
