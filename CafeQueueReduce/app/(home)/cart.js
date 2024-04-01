62d174f9dbc6a11c0c5f948617bbdf26e2d17d3a
yakir <158854050+YakirDray@users.noreply.github.com>
2024-04-01 16:08:47 +0300

    Revert "Merge branch 'main' of https://github.com/YakirDray/CafeQueueReduce"
    This reverts commit d32572d479e16a0aa5e39114ab73612ddcd49fce, reversing
    changes made to c89d0a5ac691e39d10954a38d966261334dab875.

--- CafeQueueReduce/app/(home)/cart.js
+++ CafeQueueReduce/app/(home)/cart.js
@@ -14,12 +14,9 @@ import { Entypo } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import fechData from "../(home)/index";
 const cart = () => {
-
-
   const params = useLocalSearchParams();
   const router = useRouter();
   const cart = useSelector((state) => state.cart.cart);
-
   const dispatch = useDispatch();
   const instructions = [{ id: 1, name: "עד הכיתה", iconName: "door-open" }];
   const total = cart
@@ -27,11 +24,8 @@ const cart = () => {
     .reduce((curr, prev) => curr + prev, 0);
   console.log(cart);
   fechData(cart.cart);
- 
-  
   return (
     <>
-    
       <ScrollView style={{ padding: 10, flex: 1, backgroundColor: "#F0F8FF" }}>
         <View style={{ flexDirection: "row", alignItems: "center", gap: 8 }}>
           <Ionicons name="arrow-back" size={24} color="black" />
@@ -444,5 +438,5 @@ const cart = () => {
     </>
   );
 };
-export default cart;
 
+export default cart;
