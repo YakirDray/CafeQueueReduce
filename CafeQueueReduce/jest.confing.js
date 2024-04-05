6e8dbea667489fdd9b04203526027c3e4163f53b
yakir <158854050+YakirDray@users.noreply.github.com>
2024-04-05 13:19:38 +0300

    h

--- CafeQueueReduce/jest.confing.js
+++
@@ -1,6 +0,0 @@
-export const preset = 'jest-expo';
-export const setupFilesAfterEnv = ['@testing-library/jest-native/extend-expect'];
-export const transformIgnorePatterns = [
-    "node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?)/)"
-];
-  
